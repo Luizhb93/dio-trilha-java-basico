697d22d11ca73ab69117945cc358123c54f90e29
Luiz Brito <luiz.brito@inema.ba.gov.br>
2024-09-04 23:20:39 -0300

    Iniciando o projto ContaBanco

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,18 @@
+public class ContaTerminal {
+
+    int numero;
+    String agencia;
+    String nomeCliente;
+    Double saldo;
+
+    public static void main(String[] args) {
+        //TODO:Conhecer e importar a classe scaner
+
+        //Exibir as mensagens para o nosso Usuário
+
+        //Obter pela scanner os valores digitados no terminal
+
+        //Exibir a mensagem conta criada
+    }
+    
+}
